8ff4a973ab734ee8453e292a27bbf54b66878331
Lurdes21 <59867958+Lurdes21@users.noreply.github.com>
2020-06-16 10:36:47 +0200

    bye world

---
+++ src/examen_gabarroaguilera/examen_gabarro_aguilera.java
@@ -0,0 +1,21 @@
+package examen_gabarroaguilera;
+
+public class examen_gabarro_aguilera {
+
+	public static void main(String[] args) {
+	printOne();
+	printOne();
+	printTwo();
+}
+
+public static void printOne() {
+	System.out.println("Hello World");
+}
+
+public static void printTwo() {
+	printOne();
+	printOne();
+}
+}
+
+
